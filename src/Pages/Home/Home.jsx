dfbe9b95b5d5a987049c8d8fcb6cd8d9cbbaa680
tirthadas9775 <75427198+tirthadas9775@users.noreply.github.com>
2023-12-19 12:02:21 +0600

    sidebar and home banner section added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,21 @@
+import bannerImg from '../../assets/banner-final-transparent.gif';
+import bannerBg from '../../assets/banner-bg.jpg';
 const Home = () => {
     return (
         <div>
-            <h1>This is HOme</h1>
+            <div className="hero min-h-screen" style={{ backgroundImage: `url(${bannerBg})` }}>
+                <div className="hero-overlay bg-opacity-75"></div>
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className='flex-1 flex justify-center items-center'>
+                        <img src={bannerImg} className="" />
+                    </div>
+                    <div className='flex-1 text-[#e5e5e5]'>
+                        <h1 className="text-5xl font-bold">Make your survey more convenient with Survey360!</h1>
+                        <p className="py-6">Welcome to Survey360, where every voice matters. Dive into a world of thought-provoking surveys designed to capture your unique perspectives.</p>
+                        <button className="btn bg-[#fca311] text-white border-0 hover:text-[#fca311]">Explore Now</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,6 +1,6 @@
 import { NavLink, Outlet } from "react-router-dom";
 import { RiMenu2Fill } from "react-icons/ri";
-import logo from '../assets/logo.png';
+import logo from '../assets/logoWhite.png';
 
 const Main = () => {
     return (
@@ -8,16 +8,17 @@ const Main = () => {
             <div className="drawer lg:drawer-open">
                 <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
                 <div className="drawer-content">
-                    <label htmlFor="my-drawer-2" className="btn fixed top-2 right-2 drawer-button lg:hidden"><RiMenu2Fill /></label>
+                    <label htmlFor="my-drawer-2" className="btn fixed z-50 top-2 right-2 drawer-button lg:hidden"><RiMenu2Fill /></label>
                     <Outlet></Outlet>
                 </div>
                 <div className="drawer-side">
                     <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
-                    <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+                    <ul className="menu p-4 w-72 min-h-full bg-[#212529] text-white">
                         {/* Sidebar content here */}
                         <img src={logo} alt="" className="w-[150px] mx-auto" />
                         <li><NavLink to='/'>Home</NavLink></li>
-                        <li><NavLink to="/add-survey">Add Survey</NavLink></li>
+                        <li><NavLink to="/surveys">All Surveys</NavLink></li>
+                        <li><NavLink to="/pricing">Pricing</NavLink></li>
                     </ul>
                 </div>
             </div>
