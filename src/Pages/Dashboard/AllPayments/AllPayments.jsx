e4cde6da204b9cc526186509a0795fa7a862a3ff
tirthadas9775 <75427198+tirthadas9775@users.noreply.github.com>
2024-01-08 23:29:30 +0600

    all payments route added in admin route

--- src/Pages/Dashboard/AllPayments/AllPayments.jsx
+++ src/Pages/Dashboard/AllPayments/AllPayments.jsx
@@ -1,7 +1,45 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+
 const AllPayments = () => {
+    const axiosSecure = useAxiosSecure();
+    const { data: payments = [], refetch } = useQuery({
+        queryKey: ["payments"],
+        queryFn: async () => {
+            const res = await axiosSecure.get('/payments', { withCredentials: true });
+            return res.data;
+        }
+    })
     return (
         <div>
-            
+            <div className="my-8">
+                <h1 className="text-4xl font-bold text-center">All Payments</h1>
+            </div>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>Serial No.</th>
+                            <th>Plan Name</th>
+                            <th>Customer Email</th>
+                            <th>Transaction ID</th>
+                            <th>Paid Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            payments.map((payment, idx) => <tr>
+                                <th>{idx + 1}</th>
+                                <td>{payment?.product?.name}</td>
+                                <td>{payment?.email}</td>
+                                <td>{payment?.transactionId}</td>
+                                <td className={`${payment.paidStatus ? "text-green-500" : "text-amber-600"}`}>{payment?.paidStatus ? "Paid" : "Due"}</td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
